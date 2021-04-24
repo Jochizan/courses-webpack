07aae18e4f65c5d6b53da76e0143ee30754134af
Joan <69653003+Jochizan@users.noreply.github.com>
2021-04-24 13:31:22 -0500

    Starting the webpack course

---
+++ foo/src/index.js
@@ -0,0 +1,1 @@
+const getAll = () => fetch('http://localhost:5001');
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import style from './style.css';
+
+const arr = [1, 2, 3, 4, 5];
+
+const codeESNext = () => console.log(...arr);
+
+console.log('Hola mundo sin configuración con Webpack ❤');
+
+codeESNext();
